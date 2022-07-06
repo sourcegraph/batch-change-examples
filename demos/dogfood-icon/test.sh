7fa6683ba1aaf0a84e67579bad885045bd5d29ea
Malo Marrec <malo.marrec@gmail.com>
2022-07-06 18:25:26 +0200

    cleanup

--- demos/dogfood-icon/test.sh
+++
@@ -1,5 +0,0 @@
-if [[ $(pwd) = "/work" ]]; then
-        echo "This is the root workspace, skipping"
-      else
-        yarn --cwd / transform --write -t 'packages/transforms/src/mdiIconToMdiPath/mdiIconToMdiPath.ts' "${PWD}/**/*.tsx"
-      fi
